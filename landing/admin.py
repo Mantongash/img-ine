c2b2c70c0e4fd9f8667b505d5e778ba005bcb971
Mantongash <you@example.com>
2020-02-03 11:29:40 +0300

    Registered the models in admin.py

--- landing/admin.py
+++ landing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Location, Categories
+
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Categories)
 
 # Register your models here.

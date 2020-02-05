544f5b8b71e16cb8afdb7bf21fee37fb5b03c5d1
Mantongash <you@example.com>
2020-02-05 14:09:25 +0300

    fix issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = "/images/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
